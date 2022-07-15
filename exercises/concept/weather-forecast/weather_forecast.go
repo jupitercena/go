80fc63b12d29b778407f89dcf10fa5e422cf2087
Jupiter Cena <jupitercena@gmail.com>
2022-07-15 22:55:48 +0800

    Update weather_forecast.go

--- exercises/concept/weather-forecast/weather_forecast.go
+++ exercises/concept/weather-forecast/weather_forecast.go
@@ -1,8 +1,9 @@
-package weather
+package weather // This package contents weather forcasting
 
-var CurrentCondition string
-var CurrentLocation string
+var CurrentCondition string // To hold the current condition
+var CurrentLocation string // To hold the current location
 
+// This function to execute the weather forcasted based on the current condition and current location
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
